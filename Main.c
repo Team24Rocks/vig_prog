04c878936f6b9f4ed14d540626494c6a9b2d0be2
alkruege <alkruege@asu.edu>
2014-03-16 21:34:24 -0700

    changed return

--- Main.c
+++ Main.c
@@ -81,12 +81,12 @@ int main(int   pArgc, char *pArgv[]) {
     ControllerEnd();
 
     /* What should main return? */
-    if(pArgc!=0){
-        char *pFmt=("An error code %d occurred in %s", err_code, some_string_var);
-        MainTerminate(pArgc,pFmt);
-        return 1;
-    }
-    else
+    //if(pArgc!=0){
+      //  char *pFmt=("An error code %d occurred in %s", err_code, some_string_var);
+        //MainTerminate(pArgc,pFmt);
+       // return 1;
+    //}
+    //else
         return 0;
 }
 
